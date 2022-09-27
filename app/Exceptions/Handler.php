8346f35ef9363ad955d885ed3fec3180e7c4f9fa
rawdasherif119 <rawdasherif119@gmail.com>
2022-09-27 04:18:49 +0200

    Handle exceptions & configer auth api gaurd

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,16 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Http\Response;
+use App\Http\Resources\ErrorResource;
+use Illuminate\Validation\UnauthorizedException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -44,7 +52,30 @@ class Handler extends ExceptionHandler
     public function register()
     {
         $this->reportable(function (Throwable $e) {
-            //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            abort(new ErrorResource(Response::HTTP_METHOD_NOT_ALLOWED, __('errors.method_not_allowed')));
+        }
+        if ($exception instanceof NotFoundHttpException) {
+            abort(new ErrorResource(Response::HTTP_NOT_FOUND, __('errors.route_not_found')));
+        }
+        if ($exception instanceof ModelNotFoundException) {
+            abort(new ErrorResource(Response::HTTP_NOT_FOUND, __('errors.model_not_found')));
+        }
+        if (
+            $exception instanceof UnauthorizedException
+            || $exception instanceof AuthorizationException
+        ) {
+            abort(new ErrorResource(Response::HTTP_FORBIDDEN, __('errors.unauthorized')));
+        }
+
+        if ($exception instanceof BadRequestHttpException) {
+            abort(new ErrorResource(Response::HTTP_BAD_REQUEST, $exception->getMessage()));
+        }
+        return parent::render($request, $exception);
     }
 }
